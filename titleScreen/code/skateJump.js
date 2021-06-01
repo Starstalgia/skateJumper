0cc11eea3d112675b022e09658d3704c0c1e8bee
athor <athornburg@millaschool.org>
2021-06-01 14:00:17 -0700

    A lot sorry

--- titleScreen/code/skateJump.js
+++ titleScreen/code/skateJump.js
@@ -1,41 +1,39 @@
-
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
 var skateboardRight= 35;
 var skateboardDown= 440;
 let baseline=465;
-let maxHeight=5;
+let maxHeight=10;
 
 
 window.onload = function() {
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    // ctx.translate(0,250);
-    // ctx.scale(1,-1);
+    drawSkateboard()
+
+};
+
+function drawSkateboard() {
     var img = document.getElementById("skateboard");
     ctx.drawImage(img, skateboardRight, skateboardDown, 100, 25);
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
     ctx.beginPath();
     ctx.moveTo(0, baseline);
     ctx.lineTo(600, baseline);
     ctx.stroke();
-};
-
-
+}
 
 
 document.addEventListener('DOMContentLoaded',() => {
-    const box=document.querySelector('.box')
+    const skateboard=document.querySelector('.skateboard')
     const gameDisplay=document.querySelector('.game-container')
     const ground= document.querySelector('.street')
 
-    let boxLeft= 100
-    let boxBottom= 0
+    let skateboardLeft= 100
+    let skateboardBottom= 0
     let gravity= 2
 
     function startGame() {
         if (skateboardDown > 6) skateboardDown -= gravity
-        box.style.bottom= boxBottom + 'px'
-        box.style.left= boxLeft + 'px'
+        skateboard.style.bottom= skateboardBottom + 'px'
+        skateboard.style.left= skateboardLeft + 'px'
     }
     let timerId= setInterval(startGame,20)
     function control (e) {
@@ -45,12 +43,13 @@ document.addEventListener('DOMContentLoaded',() => {
 
     }
     function jump() {
-       if (skateboardDown < maxHeight) skateboardDown -=50
-        skateboardDown -=50
-        console.log ()
-    }
-    document.addEventListener('keyup',control)
 
+      skateboardDown -=50;
+        drawSkateboard();
+
+    }
+    document.addEventListener('keyup',jump);
+})
 
     // function generateObstacle() {
     //    const obstacle = document.createElement('div');
@@ -60,3 +59,5 @@ document.addEventListener('DOMContentLoaded',() => {
     // }
     // generateObstacle()
 })
+// console.log ()
+// if (skateboardDown < maxHeight) skateboardDown -=50
